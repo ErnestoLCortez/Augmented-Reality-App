96c78af483f9377eaf7d5fb4aba9e0c4e85099d6
Ernesto Cortez <ernestoluiscortez@gmail.com>
2017-03-18 01:17:46 -0700

    refactored header

--- src/app.js
+++ src/app.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
+import { Container } from 'native-base';
+import AppHeader from './components/Header';
 
 export default class App extends Component {
   render() {
     return (
       <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-          <Title>TerraSite???</Title>
-          </Body>
-          <Right>
-          </Right>
-        </Header>
+        <AppHeader />
       </Container>
     );
   }
