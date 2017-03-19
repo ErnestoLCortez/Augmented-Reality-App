ff7d363feef6b06b8bd29d28375a13216a7189c9
Michael Fernandez <mike@Michaels-MacBook-Pro.local>
2017-03-18 21:01:38 -0700

    Firebase is connected. Auth enabled for hardkeyed test users and Google.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,15 @@ import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+    componentWillMount() {
+      firebase.initializeApp({
+      apiKey: 'AIzaSyB8RsM5NUFI_YQG9NXEhWntEZ8pMci-SmE',
+      authDomain: 'terrasite-58680.firebaseapp.com',
+      databaseURL: 'https://terrasite-58680.firebaseio.com',
+      storageBucket: 'terrasite-58680.appspot.com',
+      messagingSenderId: '292158142746'
+    });
+    }
   render() {
     return (
       <Container>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Container, Content, Form, Item, Input, Label, Button, Text, Card } from 'native-base';
 import { FormInput } from './FormInput';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: ''
   };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+      firebase.auth().signInWithEmailAndPassword(email, password);
+  }
 
   render() {
         return (
@@ -17,18 +23,18 @@ class LoginForm extends Component {
                       <FormInput
                           labelProp='email'
                           value={this.state.email}
-                          onChangeText={ emailText => this.setState({ email }) }
+                          onChangeText={ email => this.setState({ email }) }
                       />
                   </Card>
                   <Card>
                       <FormInput
                           labelProp='password'
                           value={this.state.password}
-                          onChangeText={ passwordText => this.setState({ password }) }
+                          onChangeText={ password => this.setState({ password }) }
                       />
                   </Card>
                         <Card>
-                            <Button full success>
+                            <Button full success onPress={this.onButtonPress.bind(this)}>
                                 <Text>Log In</Text>
                             </Button>
                         </Card>
