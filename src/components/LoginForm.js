fc4a3da45fa43724987c4790cd63895388c4e5f4
Michael Fernandez <mike@Michaels-MacBook-Pro.local>
2017-03-18 23:42:16 -0700

    onGoogleButtonPress() function working in the LoginForm component is non-functional. Only login with test user is functional.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -20,6 +20,30 @@ class LoginForm extends Component {
       });
   }
 
+
+  onGoogleButtonPress() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('profile');
+
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+
+        alert(user);
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
+
   render() {
         return (
                 <Content>
@@ -40,7 +64,10 @@ class LoginForm extends Component {
                   </Card>
                         <Card>
                             <Button full success onPress={this.onButtonPress.bind(this)}>
-                                <Text>Log In</Text>
+                                <Text>Log in</Text>
+                            </Button>
+                            <Button full info onPress={this.onGoogleButtonPress.bind(this)}>
+                                <Text>Log in with Google</Text>
                             </Button>
                         </Card>
                     </Form>
