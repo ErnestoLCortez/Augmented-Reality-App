fc617a95b123ec3f70f8cc31fdbf87d5946916f4
Michael Fernandez <micfernandez@csumb.edu>
2017-03-20 00:56:17 -0700

    Added functioning Camera component. Untested outside of emulator. Needs verification on device.

--- src/components/CameraView.js
+++ src/components/CameraView.js
@@ -1,12 +1,57 @@
+'use strict';
 import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { Container, Content } from 'native-base';
+import Camera from 'react-native-camera';
+
+class BasicCamera extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+      </View>
+    );
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
 
 export default class CameraView extends Component {
     render() {
         return (
             <Container>
-                <Content padder>
-                </Content>
+                <BasicCamera />
             </Container>
         );
     }
