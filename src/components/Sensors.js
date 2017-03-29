6637a5f1f1054c66ee64d9d47567891efcb82a19
Michael Fernandez <micfernandez@csumb.edu>
2017-03-28 21:53:33 -0700

    ref react-native-sensors

---
+++ src/components/Sensors.js
@@ -0,0 +1,46 @@
+// (mostly) as pulled from react-native-sensors README file.
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Accelerometer, Gyroscope, decorator as sensors } from 'react-native-sensors';
+
+
+const accelerationObservable = new Accelerometer({
+  updateInterval: 100, // defaults to 100ms
+});
+
+// Normal RxJS functions
+accelerationObservable
+  .map(({ x, y, z }) => x + y + z)
+  .filter(speed => speed > 20)
+  .subscribe(speed => console.log(`You moved your phone with ${speed}`));
+
+setTimeout(() => {
+  accelerationObservable.stop();
+}, 1000);
+
+
+class MotManager { // no lifecycle needed
+  render() {
+    const {
+      Accelerometer,
+      Gyroscope,
+    } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Acceleration has value: {Accelerometer}
+          Gyro has value: {Gyroscope}
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default sensors({
+  Accelerometer: {
+    updateInterval: 300, // optional
+  },
+  Gyroscope: true,
+  Magnetometer: false, // disabled
+})(MotManager);
