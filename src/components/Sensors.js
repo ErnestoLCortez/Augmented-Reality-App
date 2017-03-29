f6103bdb07946b0a074805a0426750bee33d85ce
Michael Fernandez <micfernandez@csumb.edu>
2017-03-28 23:09:44 -0700

    w/o accelerationObservable

--- src/components/Sensors.js
+++ src/components/Sensors.js
@@ -1,22 +1,22 @@
 // (mostly) as pulled from react-native-sensors README file.
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { Accelerometer, Gyroscope, decorator as sensors } from 'react-native-sensors';
+import { Accelerometer, Gyroscope, ReactNativeSensors, decorator as sensors } from 'react-native-sensors';
 
 
-const accelerationObservable = new Accelerometer({
-  updateInterval: 100, // defaults to 100ms
-});
-
-// Normal RxJS functions
-accelerationObservable
-  .map(({ x, y, z }) => x + y + z)
-  .filter(speed => speed > 20)
-  .subscribe(speed => console.log(`You moved your phone with ${speed}`));
-
-setTimeout(() => {
-  accelerationObservable.stop();
-}, 1000);
+// const accelerationObservable = new ReactNativeSensors.Accelerometer({
+//   updateInterval: 100, // defaults to 100ms
+// });
+//
+// // Normal RxJS functions
+// accelerationObservable
+//   .map(({ x, y, z }) => x + y + z)
+//   .filter(speed => speed > 20)
+//   .subscribe(speed => console.log(`You moved your phone with ${speed}`));
+//
+// setTimeout(() => {
+//   accelerationObservable.stop();
+// }, 1000);
 
 
 export class MotManager { // no lifecycle needed
