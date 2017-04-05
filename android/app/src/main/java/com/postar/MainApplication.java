f96ff254efdc1081162655a4236d2e346cd4719e
Ernesto Cortez <ernestoluiscortez@gmail.com>
2017-04-05 02:12:59 -0700

    Run eject to rename app to PostAR

--- android/app/src/main/java/com/postar/MainApplication.java
+++ android/app/src/main/java/com/postar/MainApplication.java
@@ -1,10 +1,9 @@
-package com.mobileapp;
+package com.postar;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.sensors.RNSensorsPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSensorsPackage(),
-            new RCTCameraPackage()
+            new RNSensorsPackage()
       );
     }
   };
