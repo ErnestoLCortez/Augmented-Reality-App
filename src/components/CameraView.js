091ff9311b623dbbb350f2e8f567e541f2a3b806
Michael Fernandez <micfernandez@csumb.edu>
2017-04-06 22:56:48 -0700

    testing on blank view

--- src/components/CameraView.js
+++ src/components/CameraView.js
@@ -9,23 +9,37 @@ import Camera from 'react-native-camera';
 class BasicCamera extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-          // <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-          <ActionButton
-              buttonColor="rgba(231,76,60,1)"
-              onPress={() => { Toast.show({
-                  text: 'Whoop!',
-                  position: 'bottom'
-          })}}>
-            <Icon name="star-half" style={styles.actionButtonIcon} />
-          </ActionButton>
-        </Camera>
+      // <View style={styles.container}>
+      //   <Camera
+      //     ref={(cam) => {
+      //       this.camera = cam;
+      //     }}
+      //     style={styles.preview}
+      //     aspect={Camera.constants.Aspect.fill}>
+      //     // <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+      //     <ActionButton
+      //         buttonColor="rgba(231,76,60,1)"
+      //         onPress={() => { Toast.show({
+      //             text: 'Whoop!',
+      //             position: 'bottom'
+      //     })}}>
+      //       <Icon name="star-half" style={styles.actionButtonIcon} />
+      //     </ActionButton>
+      //   </Camera>
+      // </View>
+      <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
+        {/* Rest of the app comes ABOVE the action button component !*/}
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+            <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>
     );
   }
