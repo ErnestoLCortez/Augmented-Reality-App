8c940916e2cdfa8f18dc27b80f849cd4bce18d89
Tomas Hernandez <tohernandez@csumb.edu>
2017-04-08 12:13:01 -0700

    Started working on displaying posts on a feed for testing purposes, modified navigator for the new view and fixed a minor visual bug that didn't change text color for active tab

---
+++ src/components/PostingsList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Container, Content, ListItem, Text, Separator, Button } from 'native-base';
+import firebase from 'firebase';
+
+
+export default class PostingsList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render() {
+        this.getPosts();
+        return (
+            <Container>
+                <Content>
+                    <ListItem>
+                    </ListItem>
+                </Content>
+            </Container>
+        );
+    }
+    getPosts(){
+        return fetch('https://terrasite.herokuapp.com/api/arposts')
+            .then((response) => response.json())
+            .then((responseJson) =>{
+                console.log(responseJson);
+                return responseJson;
+            })
+            .catch((error) =>{
+                console.error(error);
+            });
+    }
+}
