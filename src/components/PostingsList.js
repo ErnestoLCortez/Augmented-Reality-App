e752e26d3b278be2a1849246a72cc978603a3969
Tomas Hernandez <tohernandez@csumb.edu>
2017-04-09 14:17:39 -0700

    Now rendering all posts to a postings view

--- src/components/PostingsList.js
+++ src/components/PostingsList.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
-import { Container, Content, ListItem, Text, Separator } from 'native-base';
+import { Container, Content, ListItem, Text } from 'native-base';
+import { Post } from './Post';
 
 export default class PostingsList extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.state = {
+            data: []
+        }
     }
-    render() {
-        this.getPosts();
-        return (
-            <Container>
-                <Content>
-                    <ListItem>
-                    </ListItem>
-                </Content>
-            </Container>
-        );
-    }
-    getPosts(){
-        return fetch('https://terrasite.herokuapp.com/api/arposts')
+    componentDidMount(){
+        fetch('https://terrasite.herokuapp.com/api/arposts')
             .then((response) => response.json())
             .then((responseJson) =>{
-                console.log(responseJson);
-                return responseJson;
+                this.setState({
+                    data: responseJson
+                })
             })
             .catch((error) =>{
                 console.error(error);
             });
+    }
+    render() {
+        const posts = this.state.data.map((object, index) => {
+            if(object.hasOwnProperty('name')){
+                return (
+                    <Post key={index} content={object.content} name={object.name}/>
+                );
+            }
+        });
+        return (
+            <Container>
+                {posts}
+            </Container>
+        );
     }
 }
