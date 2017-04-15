daf92a5aeb8d98918e3c82cb71c9c40d8b0360ab
Ernesto Cortez <ernestoluiscortez@gmail.com>
2017-04-15 13:48:22 -0700

    Add google-sign-in package, services json and adjust gradle settings

--- android/app/src/main/java/com/postar/MainApplication.java
+++ android/app/src/main/java/com/postar/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactlibrary.googlesignin.RNGoogleSignInPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+              new RNGoogleSignInPackage(),
             new RNDeviceInfo(),
             new RNSensorsPackage(),
             new RCTCameraPackage()
