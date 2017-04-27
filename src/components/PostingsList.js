6b91b30d93e88b8c91eb78e27e428b63b611c037
Tomas Hernandez <tohernandez@csumb.edu>
2017-04-27 16:47:40 -0700

    Blank mapview

--- src/components/PostingsList.js
+++ src/components/PostingsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'native-base';
 import { Post } from './Post';
+import MapView from 'react-native-maps';
 
 export default class PostingsList extends Component {
     constructor(props){
@@ -10,28 +11,35 @@ export default class PostingsList extends Component {
         }
     }
     componentDidMount(){
-        fetch('https://terrasite.herokuapp.com/api/arposts')
-            .then((response) => response.json())
-            .then((responseJson) =>{
-                this.setState({
-                    data: responseJson
-                })
-            })
-            .catch((error) =>{
-                console.error(error);
-            });
+        // fetch('https://terrasite.herokuapp.com/api/arposts')
+        //     .then((response) => response.json())
+        //     .then((responseJson) =>{
+        //         this.setState({
+        //             data: responseJson
+        //         })
+        //     })
+        //     .catch((error) =>{
+        //         console.error(error);
+        //     });
     }
     render() {
-        const posts = this.state.data.map((object, index) => {
-            if(object.hasOwnProperty('name')){
-                return (
-                    <Post key={index} content={object.content} name={object.name}/>
-                );
-            }
-        });
+        // const posts = this.state.data.map((object, index) => {
+        //     if(object.hasOwnProperty('name')){
+        //         return (
+        //             <Post key={index} content={object.content} name={object.name}/>
+        //         );
+        //     }
+        // });
         return (
             <Container>
-                {posts}
+            <MapView
+                initialRegion={{
+                  latitude: 37.78825,
+                  longitude: -122.4324,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+            />
             </Container>
         );
     }
