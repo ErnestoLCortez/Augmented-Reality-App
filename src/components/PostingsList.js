1e0624afb6af6e747b1a00e97731e0d5ed3c6ca5
Tomas Hernandez <tohernandez@csumb.edu>
2017-05-02 23:29:17 -0700

    Removed post component and now have working markers on map view

--- src/components/PostingsList.js
+++ src/components/PostingsList.js
@@ -1,32 +1,72 @@
 import React, { Component } from 'react';
-import {StyleSheet, Dimensions} from 'react-native'
+import { StyleSheet, Dimensions, AsyncStorage, Text } from 'react-native'
 import { Container } from 'native-base';
 import MapView from 'react-native-maps'
+
 var {height, width } = Dimensions.get('window');
-import { Post } from './Post';
 
 export default class PostingsList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            latitude: 0.0, longitude: 0.0
+            latitude: 0.0, longitude: 0.0, data: []
         }
     }
     componentDidMount(){
         navigator.geolocation.getCurrentPosition(
             (position) => {
+                var backendToken = null;
+                var firebaseToken = null;
+                try {
+                  backendToken = AsyncStorage.getItem('JWT_TOKEN');
+                  firebaseToken = AsyncStorage.getItem('ID_TOKEN');
+                } catch(err) {
+                  console.log(err);
+                }
+
                 this.setState({latitude: position['coords']['latitude'],
-                longitude: position['coords']['longitude']})
-        });
+                longitude: position['coords']['longitude']});
+
+                fetch('https://terrasite.herokuapp.com/api/arposts/' +
+                  position['coords']['latitude'] + '/' + position['coords']['longitude'] + '/0' ,{
+                  method: 'GET',
+                  headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'x-access-token': backendToken,
+                    'idtoken': firebaseToken
+                  }})
+                  .then((response) => response.json())
+                  .then((responseJson) =>{
+                    this.setState({data: responseJson})
+                  })
+                  .catch((error) =>{
+                    console.error(error);
+                  });
+            },
+            //Catch for location
+            (error) => {
+                console.log(error);
+            }
+        );
     }
+
+    componentWillUnmount(){
+        navigator.geolocation.clearWatch(this);
+    }
+
     render() {
-        // const posts = this.state.data.map((object, index) => {
-        //     if(object.hasOwnProperty('name')){
-        //         return (
-        //             <Post key={index} content={object.content} name={object.name}/>
-        //         );
-        //     }
-        // });
+        console.log(this.state.data);
+        const posts = this.state.data.map((object, index) => {
+            return <MapView.Marker key={index} coordinate={{latitude: object['location']['coordinates'][1],
+                    longitude: object['location']['coordinates'][0]}}>
+                    <MapView.Callout style={styles.plainView}>
+                        <Text>{object['content']}</Text>
+                        <Text></Text>
+                        <Text>Posted by: {object['name']}</Text>
+                    </MapView.Callout>
+                    </MapView.Marker>
+        });
         return (
             <Container>
                 <MapView style={styles.map}
@@ -36,13 +76,17 @@ export default class PostingsList extends Component {
                         latitudeDelta: 0.0042,
                         longitudeDelta: 0.0021,
                     }}>
-
+                    { posts }
                 </MapView>
             </Container>
         );
     }
 }
+
 var styles = StyleSheet.create({
+    plainView: {
+        width: 200,
+    },
   map:{
       width:width,
       height: height
